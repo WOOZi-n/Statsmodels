d349cdb84b2b86446f22ec74e696f3e6c8472059
LAPTOP-QNG3LN7H\jmjwj <jmjwj@naver.com>
2022-04-13 10:22:50 +0900

    no message

--- LSE.py
+++ LSE.py
@@ -1,16 +1,20 @@
 #LSE 구현
 import numpy as np
 
-x = list(map(int, input().split()))
-y = list(map(int, input().split()))
+x = np.array(list(map(int, input().split())))
+y = np.array(list(map(int, input().split())))
 xmean = np.mean(x)
 ymean = np.mean(y)
-sxx=0
-sxy=0
-for a,b in zip(x,y):
-    sxx += (a-xmean)**2
-    sxy += (a-xmean)*(b-ymean)
 
-print("%d x + %d = y" %((sxy/sxx), ymean-(sxy/sxx)*xmean))
+sxx = ((x - xmean)**2).sum()
+sxy = ((y - ymean)*(x-xmean)).sum()
 
 
+
+estedB1 = sxy/sxx
+estedB0 = ymean - estedB1*xmean
+print("%d x + %d = y" %(estedB1, estedB0))
+
+
+MSE = ((y-estedB1*x-estedB0)**2).mean()
+print("MSE = %d" %MSE)
